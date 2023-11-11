9d97fde8245b89b2819a33b1e45f86f9526dba9e
Alicia Lemos <alemos@leafnoise.io>
2023-11-11 13:40:33 -0300

    Fix updateUser

--- controller/userController.js
+++ controller/userController.js
@@ -44,7 +44,19 @@ class UserController {
 
     const { name, lastName, mail } = req.body;
 
-    const query = `UPDATE user SET name = "${name}", lastName = "${lastName}", mail = "${mail}" WHERE id = ${id}`;
+  // Construye la query con los datos proporcionados
+  let query = 'UPDATE user SET ';
+  const updates = [];
+  
+  if (name) updates.push(`name = "${name}"`);
+  if (lastName) updates.push(`lastName = "${lastName}"`);
+  if (mail) updates.push(`mail = "${mail}"`);
+  
+  if (updates.length === 0) {
+    return res.status(400).send({ success: false, error: 'No se proporcionaron datos para actualizar' });
+  }
+  
+  query += updates.join(', ') + ` WHERE id = ${id}`;
 
     connection.query(query).then(result => {
       res.status(200).send({ success: true, message: `Usuario con ID ${id} actualizado con éxito`}); 
